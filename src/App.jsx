6ff2b80134579e1b4b817fd9d16684aced2817a7
Muhammad Arya Wirawan <maryawirawan13@gmail.com>
2022-11-30 19:49:14 +0700

    create status & aktif program page + adjust navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import React from "react"
-import { Routes, Route } from 'react-router-dom'
-import Layout from './layout/Layout'
-import Home from './components/Home'
-import HomeOrganization from "./components/HomeOrganization"
-import Login from "./components/Login"
-import AllProgram from "./components/AllProgram"
-import DetailProgram from "./components/DetailProgram"
-import DetailProgramOrganization from "./components/DetailProgramOrganization"
-import Logout from "./components/Logout"
-import HalamanProfile from "./components/HalamanProfile"
-import PrivateLayout from "./layout/PrivateLayout"
-import RegisterApplicant from "./components/RegisterApplicant"
-import RegisterOrganization from "./components/RegisterOrganization"
-import RegisterTypeAccount from "./components/RegisterTypeAccount"
-import CreateProgram from "./components/CreateProgram"
-import EditProgram from "./components/EditProgram"
-import DetailApplicant from "./components/DetailApplicant"
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Home from "./components/Home";
+import HomeOrganization from "./components/HomeOrganization";
+import Login from "./components/Login";
+import AllProgram from "./components/AllProgram";
+import DetailProgram from "./components/DetailProgram";
+import DetailProgramOrganization from "./components/DetailProgramOrganization";
+import Logout from "./components/Logout";
+import HalamanProfile from "./components/HalamanProfile";
+import PrivateLayout from "./layout/PrivateLayout";
+import RegisterApplicant from "./components/RegisterApplicant";
+import RegisterOrganization from "./components/RegisterOrganization";
+import RegisterTypeAccount from "./components/RegisterTypeAccount";
+import CreateProgram from "./components/CreateProgram";
+import EditProgram from "./components/EditProgram";
+import DetailApplicant from "./components/DetailApplicant";
+import MyProgram from "./components/MyProgram";
+import "./assets/style.css";
+import ActivepProgram from "./components/ActiveProgram";
 function App() {
-
   return (
     <Routes>
       {/* Public Route */}
@@ -27,24 +28,31 @@ function App() {
         <Route path="/organization" element={<HomeOrganization />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<RegisterTypeAccount />} />
-         <Route path="/register-applicant" element={<RegisterApplicant />} />
+        <Route path="/register-applicant" element={<RegisterApplicant />} />
         <Route path="/createprogram" element={<CreateProgram />} />
         <Route path="/editprogram/:id" element={<EditProgram />} />
         <Route path="/allprogram" element={<AllProgram />} />
         <Route path="/detailprogram/:id" element={<DetailProgram />} />
-        <Route path="/detailprogramorganization/:id" element={<DetailProgramOrganization />} />
+        <Route
+          path="/detailprogramorganization/:id"
+          element={<DetailProgramOrganization />}
+        />
         <Route path="/detailapplicant/:id" element={<DetailApplicant />} />
-        <Route path="/logout" element={<Logout/>}/>
-        <Route path="/register-organization" element ={<RegisterOrganization/>}/>
+        <Route path="/logout" element={<Logout />} />
+        <Route
+          path="/register-organization"
+          element={<RegisterOrganization />}
+        />
+        <Route path="/my-programs" element={<MyProgram />} />
+        <Route path="/active-program" element ={<ActivepProgram/>}/>
       </Route>
-       
-       {/* Protected Route */}
-       <Route path="/" element={<PrivateLayout />}>
-         <Route path="/profile" element={<HalamanProfile/>} />
-       </Route>
+
+      {/* Protected Route */}
+      <Route path="/" element={<PrivateLayout />}>
+        <Route path="/profile" element={<HalamanProfile />} />
+      </Route>
     </Routes>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- src/components/RegisterTypeAccount.jsx
+++ src/components/RegisterTypeAccount.jsx
@@ -3,33 +3,47 @@ import { Link, useNavigate } from "react-router-dom";
 import "./../assets/style.css";
 
 const RegisterTypeAccount = () => {
-    const navigate = useNavigate()
-    // const handleButtonApplicant =()=> {
-    //     navigate('/register-applicant')
-    // }
+  const navigate = useNavigate();
+  // const handleButtonApplicant =()=> {
+  //     navigate('/register-applicant')
+  // }
   return (
     <section className="">
       <div className="row mt-2">
+        <Link to={"/login"} className="text-decoration-none px-4 py-2 mt-2 text-dark d-flex align-items-center gap-2">
+          <span class="material-symbols-outlined">arrow_back </span>
+          <span>Kembali ke halaman login</span>
+        </Link>
         {/* <h1 className="text-center">
           <Link to="/" className="text-decoration-none heading">
             AYO.RELIEVE
           </Link>
          
         </h1> */}
-         <img src={Together} className="img-login w-75 mx-auto" alt="" />
+        <img src={Together} className="img-login w-75 mx-auto" alt="" />
       </div>
       <div>
         <h1 className="title text-center mb-3">Pilih Jenis Akun</h1>
       </div>
       <div className="d-flex px-5 big-margin justify-content-center gap-4 w-auto  h-auto border-box">
-      <div>
-        {/* <button className="btn btn-type-acc"  type="button"> */}
-          <Link to={"/register-applicant"} className="text-decoration-none d-flex align-items-center justify-content-center btn btn-type-acc">Pelamar</Link>
-        {/* </button> */}
-      </div>
-      <div>
-         <Link to={"/register-organization"} className="text-decoration-none d-flex align-items-center justify-content-center btn btn-type-acc">Organisasi</Link>
-      </div>
+        <div>
+          {/* <button className="btn btn-type-acc"  type="button"> */}
+          <Link
+            to={"/register-applicant"}
+            className="text-decoration-none d-flex align-items-center justify-content-center btn btn-type-acc"
+          >
+            Pelamar
+          </Link>
+          {/* </button> */}
+        </div>
+        <div>
+          <Link
+            to={"/register-organization"}
+            className="text-decoration-none d-flex align-items-center justify-content-center btn btn-type-acc"
+          >
+            Organisasi
+          </Link>
+        </div>
       </div>
     </section>
   );
