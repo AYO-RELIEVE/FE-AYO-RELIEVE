34d33d1679e16d36f4a108507a4fd46f43ebfcbe
Bimzking1 <kingofbimo@gmail.com>
2022-12-02 10:50:55 +0700

    Our Team

---
+++ src/components/OurTeam.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import Bimo from "./../assets/team/Bimo.png"
+import Arya from "./../assets/team/Arya.png"
+import Ilham from "./../assets/team/Ilham.jpeg"
+import Zaki from "./../assets/team/Zaki.png"
+import Umar from "./../assets/team/Umar.png"
+import Dina from "./../assets/team/Dina.JPG"
+import "./../assets/style.css";
+
+const OurTeam = () => {
+  return (
+    <section className="py-5">
+      <div className="container px-4">
+        <h1 className="text-center text-md-start mb-4">Our Team</h1>
+
+        <div className="containerTeams">
+          <div className="CardContainerTeam">
+            <div className="">
+              <img
+                src={Ilham}
+                alt=""
+                className="imageTeam"
+              />
+            </div>
+            <div className="cardText">
+              <h5 className="cardTextNama">Ilham</h5>
+              <p className="cardTextJob">Frontend Developer</p>
+            </div>
+          </div>
+          <div className="CardContainerTeam">
+            <div className="">
+              <img
+                src={Arya}
+                alt=""
+                className="imageTeam"
+              />
+            </div>
+            <div className="cardText">
+              <h5 className="cardTextNama">Arya</h5>
+              <p className="cardTextJob">Frontend Developer</p>
+            </div>
+          </div>
+          <div className="CardContainerTeam">
+            <div className="">
+              <img
+                src={Bimo}
+                alt=""
+                className="imageTeam"
+              />
+            </div>
+            <div className="cardText">
+              <h5 className="cardTextNama">Bimo</h5>
+              <p className="cardTextJob">Frontend Developer</p>
+            </div>
+          </div>
+          <div className="CardContainerTeam">
+            <div className="">
+              <img
+                src={Zaki}
+                alt=""
+                className="imageTeam"
+              />
+            </div>
+            <div className="cardText">
+              <h5 className="cardTextNama">Zaki</h5>
+              <p className="cardTextJob">Backend Developer</p>
+            </div>
+          </div>
+          <div className="CardContainerTeam">
+            <div className="">
+              <img
+                src={Umar}
+                alt=""
+                className="imageTeam"
+              />
+            </div>
+            <div className="cardText">
+              <h5 className="cardTextNama">Umar</h5>
+              <p className="cardTextJob">Backend Developer</p>
+            </div>
+          </div>
+          <div className="CardContainerTeam">
+            <div className="">
+              <img
+                src={Dina}
+                alt=""
+                className="imageTeam"
+              />
+            </div>
+            <div className="cardText">
+              <h5 className="cardTextNama">Dina</h5>
+              <p className="cardTextJob">Backend Developer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default OurTeam
