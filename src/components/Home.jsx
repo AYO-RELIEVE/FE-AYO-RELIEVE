2f2775da3b512f860b839161302d6c9cf2185e51
Bimzking1 <kingofbimo@gmail.com>
2022-12-02 10:45:59 +0700

    Our Team Component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,12 +6,7 @@ import "./../assets/style.css";
 import Card from "./Card";
 import Navbar from "../layout/Navbar";
 import Company from "./../assets/Company.jpg"
-import Bimo from "./../assets/team/Bimo.png"
-import Arya from "./../assets/team/Arya.png"
-import Ilham from "./../assets/team/Ilham.jpeg"
-import Zaki from "./../assets/team/Zaki.png"
-import Umar from "./../assets/team/Umar.png"
-import Dina from "./../assets/team/Dina.jpg"
+import OurTeam from "./OurTeam";
 
 const Home = () => {
   const [program, setProgram] = useState([]);
@@ -176,92 +171,7 @@ const Home = () => {
         </button>
       </div>
 
-      <section className="py-5">
-        <div className="container px-4">
-          <h1 className="text-center text-md-start mb-4">Our Team</h1>
-
-          <div className="containerTeams">
-            <div className="CardContainerTeam">
-              <div className="">
-                <img
-                  src={Ilham}
-                  alt=""
-                  className="imageTeam"
-                />
-              </div>
-              <div className="cardText">
-                <h5 className="cardTextNama">Ilham</h5>
-                <p className="cardTextJob">Frontend Developer</p>
-              </div>
-            </div>
-            <div className="CardContainerTeam">
-              <div className="">
-                <img
-                  src={Arya}
-                  alt=""
-                  className="imageTeam"
-                />
-              </div>
-              <div className="cardText">
-                <h5 className="cardTextNama">Arya</h5>
-                <p className="cardTextJob">Frontend Developer</p>
-              </div>
-            </div>
-            <div className="CardContainerTeam">
-              <div className="">
-                <img
-                  src={Bimo}
-                  alt=""
-                  className="imageTeam"
-                />
-              </div>
-              <div className="cardText">
-                <h5 className="cardTextNama">Bimo</h5>
-                <p className="cardTextJob">Frontend Developer</p>
-              </div>
-            </div>
-            <div className="CardContainerTeam">
-              <div className="">
-                <img
-                  src={Zaki}
-                  alt=""
-                  className="imageTeam"
-                />
-              </div>
-              <div className="cardText">
-                <h5 className="cardTextNama">Zaki</h5>
-                <p className="cardTextJob">Backend Developer</p>
-              </div>
-            </div>
-            <div className="CardContainerTeam">
-              <div className="">
-                <img
-                  src={Umar}
-                  alt=""
-                  className="imageTeam"
-                />
-              </div>
-              <div className="cardText">
-                <h5 className="cardTextNama">Umar</h5>
-                <p className="cardTextJob">Backend Developer</p>
-              </div>
-            </div>
-            <div className="CardContainerTeam">
-              <div className="">
-                <img
-                  src={Dina}
-                  alt=""
-                  className="imageTeam"
-                />
-              </div>
-              <div className="cardText">
-                <h5 className="cardTextNama">Dina</h5>
-                <p className="cardTextJob">Backend Developer</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <OurTeam/>
     </>
   );
 };
